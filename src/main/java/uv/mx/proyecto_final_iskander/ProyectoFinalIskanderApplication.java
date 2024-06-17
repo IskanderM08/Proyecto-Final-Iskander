cb145515d31ddeb1ca18e1a128b3bd2055941ac5
IskanderM08 <IskanderMercader2002@gmail.com>
2024-06-17 10:30:00 -0600

    Estructura Inicial del Proyecto

---
+++ src/main/java/uv/mx/proyecto_final_iskander/ProyectoFinalIskanderApplication.java
@@ -0,0 +1,13 @@
+package uv.mx.proyecto_final_iskander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalIskanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalIskanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uv/mx/proyecto_final_iskander/ProyectoFinalIskanderApplicationTests.java
@@ -0,0 +1,13 @@
+package uv.mx.proyecto_final_iskander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalIskanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
