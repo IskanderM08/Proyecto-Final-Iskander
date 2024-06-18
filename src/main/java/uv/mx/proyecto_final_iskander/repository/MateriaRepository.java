48301a7f546ac9eb57975562b7cd0d33d34bae83
IskanderM08 <IskanderMercader2002@gmail.com>
2024-06-17 21:01:55 -0600

    Estructura Inicial del Proyecto

--- src/main/java/uv/mx/proyecto_final_iskander/repository/MateriaRepository.java
+++
@@ -1,7 +0,0 @@
-package uv.mx.proyecto_final_iskander.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import uv.mx.proyecto_final_iskander.model.Materia;
-
-public interface MateriaRepository extends MongoRepository<Materia, String> {
-}
